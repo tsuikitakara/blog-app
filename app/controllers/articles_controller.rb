810db5193502056e067c5faf45d612ac096ee17e
tsuikitakara <tkr.tik.1835@gmail.com>
2020-12-25 05:11:36 +0900

    記事を更新できるように変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,38 @@
 class ArticlesController < ApplicationController
     def index
-        @articles = Article.all
+      @articles = Article.all
     end
-
     def show
-        @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
     end
-
     def new
-        @article = Article.new
+      @article = Article.new
     end
-
     def create
-        @article = Article.new(article_params)
-        if @article.save
-            redirect_to article_path(@article), notice: '保存できました'
-        else
-            flash.now[:error] = '保存に失敗しました'
-            render :new
-        end
+      @article = Article.new(article_params)
+      if @article.save
+        redirect_to article_path(@article), notice: '保存できたよ'
+      else
+        flash.now[:error] = '保存に失敗しました'
+        render :new
+      end
     end
-
+    def edit
+      @article = Article.find(params[:id])
+    end
+  
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        redirect_to article_path(@article), notice: '更新できました'
+      else
+        flash.now[:error] = '更新できませんでした'
+        render :edit
+      end
+    end
+  
     private
     def article_params
-        params.require(:article).permit(:title, :content)
+      params.require(:article).permit(:title, :content)
     end
-end
+  end
