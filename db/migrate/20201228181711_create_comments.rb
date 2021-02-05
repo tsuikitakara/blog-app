b2d87f4e2105575a67c4060eacfc38066cea7b08
tsuikitakara <tkr.tik.1835@gmail.com>
2021-02-05 22:08:10 +0900

    カラム重複解消

--- db/migrate/20201228181711_create_comments.rb
+++ db/migrate/20201228181711_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
-def change
-  create_table :comments do |t|
-    t.references :article, null: false
-    t.text :content, null: false
-    t.timestamps
+  def change
+    create_table :comments do |t|
+      t.references :article, null: false
+      t.text :content, null: false
+      t.timestamps
+    end
   end
-end
 end
